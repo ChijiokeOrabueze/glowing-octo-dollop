89e472f0007f4784f079005f7d8853dca884ce92
Eze <thankgodchinemeze@gmail.com>
2022-08-03 13:24:12 +0100

    our changes

--- src/main/java/com/dufuna/demos/TicTacToe2.java
+++ src/main/java/com/dufuna/demos/TicTacToe2.java
@@ -3,4 +3,29 @@ package com.dufuna.demos;
 public class TicTacToe2
 {
 
+    String PLAYER = "X";
+    String[][] board = new String[3][3];
+
+    boolean status = false;
+
+    public void play(int row, int col)
+    {
+        if(board[row][col] == null)
+        {
+            board[row][col] = PLAYER;
+
+            if (PLAYER == "X")
+                PLAYER = "O";
+            else
+                PLAYER = "X";
+
+            for (String[] rowTable : board) {
+                if(rowTable[0] == rowTable[1] &&
+                    rowTable[1] == rowTable[2] &&
+                    rowTable[2] != null){
+                    status = true;
+                }
+            }
+        }
+    }
 }
--- src/test/java/com/dufuna/demos/TicTacToeTest2.java
+++ src/test/java/com/dufuna/demos/TicTacToeTest2.java
@@ -1,6 +1,78 @@
 package com.dufuna.demos;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+
+/**
+ * • X always goes first
+ * • Players alternate placing X's and O's on the board
+ * • Players cannot play on a played position
+ * • A player with three X's or O's in a row (horizontally, vertically, or diagonally) wins
+ * • If all nine squares are filled and neither player achieves three in a row, the game is
+ * a draw
+ */
 public class TicTacToeTest2
 {
+
+    @Test
+    void does_X_always_go_first()
+    {
+        TicTacToe2 tictac = new TicTacToe2();
+        String expected= "X";
+        String actual = tictac.PLAYER;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void players_ShouldAlternate()
+    {
+        TicTacToe2 tictac = new TicTacToe2();
+        String player1= tictac.PLAYER;
+        tictac.play(1,2);
+        String player2 = tictac.PLAYER;
+        assertNotEquals(player1, player2);
+    }
+
+    @Test
+    void can_players_play_on_a_played_position1()
+    {
+        TicTacToe2 tictac = new TicTacToe2();
+        String player1= tictac.PLAYER;
+        tictac.play(1,2);
+        String player2 = tictac.PLAYER;
+        tictac.play(1,2);
+        assertNotEquals(player1, player2);
+    }
+
+    @Test
+    void can_players_play_on_a_played_position2()
+    {
+        TicTacToe2 tictac = new TicTacToe2();
+        tictac.play(1,2);
+        tictac.play(1,2);
+        tictac.play(1,0);
+        String player4 = tictac.PLAYER;
+        tictac.play(1,2);
+        String player5 = tictac.PLAYER;
+        assertEquals(player4, player5);
+    }
+
+    @Test
+    void checks_for_horizontal_win()
+    {
+        TicTacToe2 tictac = new TicTacToe2();
+        tictac.play(0,0);
+        tictac.play(1,0);
+        tictac.play(0,1);
+        tictac.play(2,1);
+        tictac.play(0,2);
+        boolean status = tictac.status;
+
+        assertEquals(true, status);
+    }
+
 
 }
